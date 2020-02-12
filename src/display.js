9ce69316524cd3d80b561a9418c04b1d485bf161
KerronKing <king_of_palmiste@hotmail.com>
2020-02-12 14:23:31 -0400

    chore: Create App and Display components

---
+++ src/display.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Display = props => {
+  const { result } = props;
+  return (
+    <div>
+      <p>{result}</p>
+    </div>
+  );
+};
+
+Display.propTypes = {
+  result: PropTypes.string,
+};
+Display.defaultProps = {
+  result: 0,
+};
