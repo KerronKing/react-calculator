f695b132c0f42b4ce6e2dd9469f6c3637af19b50
KerronKing <king_of_palmiste@hotmail.com>
2020-02-12 15:48:18 -0400

    refactor: edit button component logic

--- src/button.js
+++ src/button.js
@@ -4,16 +4,12 @@ import PropTypes from 'prop-types';
 const Button = props => {
   const { name } = props;
   return (
-    <div name={name}>{name}</div>
+    <div>{name}</div>
   );
 };
 
 Button.propTypes = {
-  name: PropTypes.string,
-};
-
-Button.defaultProps = {
-  name: '',
+  name: PropTypes.string.isRequired,
 };
 
 export default Button;
