ac0ee7e986de55e830d3cfca92bb117241314422
KerronKing <king_of_palmiste@hotmail.com>
2020-02-12 21:32:15 -0400

    chore: update app import statement in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Components/app';
+import App from './components/app';
 import './styles.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
