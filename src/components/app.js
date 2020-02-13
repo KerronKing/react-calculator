3e78a828d48b04c2d58af0433e5c90c6d30a243c
KerronKing <king_of_palmiste@hotmail.com>
2020-02-13 18:20:26 -0400

    feat: add handleClick functions to app and button panel components

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import Display from './display';
 import ButtonPanel from './button-panel';
-// import method from '../logic/calculate';
+import method from '../logic/calculate';
 
-const App = () => (
-  <div id="app">
-    <Display />
-    <ButtonPanel />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const { state } = this.state;
+    const obj = method.calculate(state, buttonName);
+    this.setState({
+      total: obj.total,
+      next: obj.next,
+      operation: buttonName,
+    });
+  }
+
+  render() {
+    const { total, next, operation } = this.state;
+    return (
+      <div id="app">
+        <Display result={total} />
+        <ButtonPanel onClick={this.handleClick} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/button-panel.js
+++ src/components/button-panel.js
@@ -1,38 +1,50 @@
 import React from 'react';
 import Button from './button';
 
-const ButtonPanel = () => (
-  <div>
-    <div className="panel-row">
-      <Button name="AC" color="white" />
-      <Button name="+/-" color="white" />
-      <Button name="%" color="white" />
-      <Button name="/" />
-    </div>
-    <div className="panel-row">
-      <Button name="7" color="white" />
-      <Button name="8" color="white" />
-      <Button name="9" color="white" />
-      <Button name="X" />
-    </div>
-    <div className="panel-row">
-      <Button name="4" color="white" />
-      <Button name="5" color="white" />
-      <Button name="6" color="white" />
-      <Button name="-" />
-    </div>
-    <div className="panel-row">
-      <Button name="1" color="white" />
-      <Button name="2" color="white" />
-      <Button name="3" color="white" />
-      <Button name="+" />
-    </div>
-    <div className="panel-row">
-      <Button name="0" color="white" wide />
-      <Button name="." color="white" />
-      <Button name="=" />
-    </div>
-  </div>
-);
+class ButtonPanel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(buttonName) {
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="panel-row">
+          <Button name="AC" color="white" onClick={this.handleClick} />
+          <Button name="+/-" color="white" />
+          <Button name="%" color="white" />
+          <Button name="/" />
+        </div>
+        <div className="panel-row">
+          <Button name="7" color="white" />
+          <Button name="8" color="white" />
+          <Button name="9" color="white" />
+          <Button name="X" />
+        </div>
+        <div className="panel-row">
+          <Button name="4" color="white" />
+          <Button name="5" color="white" />
+          <Button name="6" color="white" />
+          <Button name="-" />
+        </div>
+        <div className="panel-row">
+          <Button name="1" color="white" />
+          <Button name="2" color="white" />
+          <Button name="3" color="white" />
+          <Button name="+" />
+        </div>
+        <div className="panel-row">
+          <Button name="0" color="white" wide />
+          <Button name="." color="white" />
+          <Button name="=" />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ButtonPanel;
