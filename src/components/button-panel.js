042343510670b3828ce6cce7d6b2325ceba86a02
KerronKing <king_of_palmiste@hotmail.com>
2020-02-12 21:19:27 -0400

    style: complete app styling

--- src/components/button-panel.js
+++ src/components/button-panel.js
@@ -4,32 +4,32 @@ import Button from './button';
 const ButtonPanel = () => (
   <div>
     <div className="panel-row">
-      <Button name="AC" />
-      <Button name="+/-" />
-      <Button name="%" />
+      <Button name="AC" color="white" />
+      <Button name="+/-" color="white" />
+      <Button name="%" color="white" />
       <Button name="/" />
     </div>
     <div className="panel-row">
-      <Button name="7" />
-      <Button name="8" />
-      <Button name="9" />
+      <Button name="7" color="white" />
+      <Button name="8" color="white" />
+      <Button name="9" color="white" />
       <Button name="X" />
     </div>
     <div className="panel-row">
-      <Button name="4" />
-      <Button name="5" />
-      <Button name="6" />
+      <Button name="4" color="white" />
+      <Button name="5" color="white" />
+      <Button name="6" color="white" />
       <Button name="-" />
     </div>
     <div className="panel-row">
-      <Button name="1" />
-      <Button name="2" />
-      <Button name="3" />
+      <Button name="1" color="white" />
+      <Button name="2" color="white" />
+      <Button name="3" color="white" />
       <Button name="+" />
     </div>
     <div className="panel-row">
-      <Button name="0" />
-      <Button name="." />
+      <Button name="0" color="white" wide />
+      <Button name="." color="white" />
       <Button name="=" />
     </div>
   </div>
--- src/components/button.js
+++ src/components/button.js
@@ -2,14 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = props => {
-  const { name } = props;
+  const { name, color, wide } = props;
+  const classes = color === 'white' && wide ? 'white wide' : color;
   return (
-    <div>{name}</div>
+    <div className={classes}>{name}</div>
   );
 };
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
+};
+Button.defaultProps = {
+  color: 'orange',
+  wide: false,
 };
 
 export default Button;
